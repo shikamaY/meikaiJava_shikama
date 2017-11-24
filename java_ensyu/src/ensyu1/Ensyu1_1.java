b3ee6c5806077e12ac95b91b5e7b8ff993e02c60
yuto-PC\yuto <ddt.shikama@gmail.com>
2017-11-24 19:52:41 +0900

    演習1-1 完了

---
+++ java_ensyu/src/ensyu1/Ensyu1_1.java
@@ -0,0 +1,11 @@
+package ensyu1;
+
+public class Ensyu1_1 {
+
+	public static void main(String[] args) {
+		// セミコロンがないため、コンパイルエラー
+		// System.out.println("hell")
+		System.out.println("hell");
+	}
+
+}
