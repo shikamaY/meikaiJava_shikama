cbf05bcc7ed9467dacc80749d1fac46ae3ffb934
yuto-PC\yuto <ddt.shikama@gmail.com>
2017-11-26 13:48:55 +0900

    演習2-5 コミットコメントの演習番号が間違っていたため再コミット

--- java_ensyu/src/ensyu2/Ensyu2_5.java
+++ java_ensyu/src/ensyu2/Ensyu2_5.java
@@ -9,11 +9,11 @@ public class Ensyu2_5 {
 		Scanner stdIn = new Scanner(System.in);
 		// xの値を入力を促すコメント表示する
 		System.out.print("xの値:");
-		// 入力値をxに代入する
+		// 入力値をdouble型xに代入する
 		double x  = stdIn.nextDouble();
 		// yの値を入力を促すコメント表示する
 		System.out.print("yの値:");
-		// 入力値をyに代入する
+		// 入力値をdouble型yに代入する
 		double y  = stdIn.nextDouble();
 		// xとyの合計値を出力する
 		System.out.println("合計は" + (x + y) + "です。" );
