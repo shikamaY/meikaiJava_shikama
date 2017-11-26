278ec0068bb458dcd910f7c405ac26d3ce992694
yuto-PC\yuto <ddt.shikama@gmail.com>
2017-11-27 00:14:03 +0900

    演習3-4 if分にスペースを追加

--- java_ensyu/src/ensyu3/Ensyu3_4.java
+++ java_ensyu/src/ensyu3/Ensyu3_4.java
@@ -16,11 +16,11 @@ public class Ensyu3_4 {
 		// 入力値をint型のbに代入する
 		int b  = stdIn.nextInt();
 		// aが大きいか判定する
-		if(a > b){
+		if(a > b) {
 			// aが大きいと表示する
 			System.out.println("aのほうが大きいです");
 		// bが大きいか判定する
-		} else if (b > a){
+		} else if (b > a) {
 			// bが大きいと表示する
 			System.out.println("bのほうが大きいです");
 		// a,bに差がなければa=bとする
