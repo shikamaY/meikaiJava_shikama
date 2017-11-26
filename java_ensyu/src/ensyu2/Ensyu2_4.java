66072401393714440d809482565440a65f5197fb
yuto-PC\yuto <ddt.shikama@gmail.com>
2017-11-26 13:16:43 +0900

    演習2-4 完了

---
+++ java_ensyu/src/ensyu2/Ensyu2_4.java
@@ -0,0 +1,22 @@
+package ensyu2;
+
+import java.util.Scanner;
+
+public class Ensyu2_4 {
+
+	public static void main(String[] args) {
+		// 入力値を代入するnumをint型で宣言
+		int num;
+		// 入力された値を入れる変数を宣言する
+		Scanner stdIn = new Scanner(System.in);
+		// 入力を促すコメント表示する
+		System.out.print("整数値:");
+		// 入力値をnumに代入する
+		num = stdIn.nextInt();
+		// 入力値に10加算した結果を表示する
+		System.out.println("10を加えた値は" + (num + 10) + "です。");
+		// 入力値を10減算した結果を表示する
+		System.out.println("10を減じた値は" + (num - 10) + "です。");
+	}
+
+}
