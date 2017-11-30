01bf56b7e9ebd54b76ae4f6cd48a232565356b68
yuto-PC\yuto <ddt.shikama@gmail.com>
2017-12-01 01:22:39 +0900

    演習3-18 完了

---
+++ java_ensyu/src/ensyu3/Ensyu3_18.java
@@ -0,0 +1,91 @@
+package ensyu3;
+
+import java.util.Scanner;
+
+public class Ensyu3_18 {
+
+	public static void main(String[] args) {
+		// Scannerクラスのオブジェクトを生成
+		Scanner stdIn = new Scanner(System.in);
+		// 入力を促すコメント表示する
+		System.out.print("月:");
+		// 入力値をint型monthに代入する
+		int month = stdIn.nextInt();
+		// numの数値から条件分岐を行う
+		switch (month) {
+		// monthが1の場合
+		case 1:
+			// 冬と表示する
+			System.out.println("冬");
+			// 分岐を抜ける
+			break;
+		// monthが2の場合
+		case 2:
+			// 冬と表示する
+			System.out.println("冬");
+			// 分岐を抜ける
+			break;
+		// monthが3の場合
+		case 3:
+			// 春と表示する
+			System.out.println("春");
+			// 分岐を抜ける
+			break;
+		// monthが3の場合
+		case 4:
+			// 春と表示する
+			System.out.println("春");
+			// 分岐を抜ける
+			break;
+		// monthが5の場合
+		case 5:
+			// 春と表示する
+			System.out.println("春");
+			// 分岐を抜ける
+			break;
+		// monthが6の場合
+		case 6:
+			// 夏と表示する
+			System.out.println("夏");
+			// 分岐を抜ける
+			break;
+		// monthが7の場合
+		case 7:
+			// 夏と表示する
+			System.out.println("夏");
+			// 分岐を抜ける
+			break;
+		// monthが8の場合
+		case 8:
+			// 夏と表示する
+			System.out.println("夏");
+			// 分岐を抜ける
+			break;
+		// monthが9の場合
+		case 9:
+			// 秋と表示する
+			System.out.println("秋");
+			// 分岐を抜ける
+			break;
+		// monthが10の場合
+		case 10:
+			// 秋と表示する
+			System.out.println("秋");
+			// 分岐を抜ける
+			break;
+		// monthが11の場合
+		case 11:
+			// 冬と表示する
+			System.out.println("冬");
+			// 分岐を抜ける
+			break;
+		// monthが12の場合
+		case 12:
+			// 冬と表示する
+			System.out.println("冬");
+			// 分岐を抜ける
+			break;
+		}
+	}
+
+}
