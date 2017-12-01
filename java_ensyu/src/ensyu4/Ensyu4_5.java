7dbefeb70d74a39e06f3214de0dada16c523c4aa
yuto-PC\yuto <ddt.shikama@gmail.com>
2017-12-01 20:38:42 +0900

    演習4-5 完了

---
+++ java_ensyu/src/ensyu4/Ensyu4_5.java
@@ -0,0 +1,28 @@
+package ensyu4;
+
+import java.util.Scanner;
+
+/*演習4-5
+ * List4-5のx--がx++となっていたら、どのような出力が得られるかを検討せよ。
+ * プログラムを作成して実行結果を確認すること
+ */
+public class Ensyu4_5 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("カウントダウンします。");
+		int x;
+		do {
+			System.out.println("正の整数値");
+			x = stdIn.nextInt();
+		} while (x <= 0);
+
+		while (x >= 0) {
+			//System.out.println(x--);	// xの値を表示してデクリメント
+			// xが0以下とならないため無限にループする
+			System.out.println(x++);
+		}
+	}
+
+}
