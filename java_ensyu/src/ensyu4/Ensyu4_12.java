b78fb2aca391a23e79689403d775b7cc1525932a
yuto-PC\yuto <ddt.shikama@gmail.com>
2017-12-08 20:37:08 +0900

    演習4-12 完了

---
+++ java_ensyu/src/ensyu4/Ensyu4_12.java
@@ -0,0 +1,35 @@
+package ensyu4;
+
+import java.util.Scanner;
+
+/* 演習4-12
+ * 前問とは逆に、0から正の整数値までカウントアップするプログラムを作成せよ。
+ */
+
+public class Ensyu4_12 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("カウントダウンします。");
+		int x = 0;
+
+		// 正数が入れられるまでループ
+		for (int i = 0; i < 1; i++){
+			System.out.println("正の整数値：");
+			x = stdIn.nextInt();
+			// 負数の場合はiを減らす
+			if (x < 0) {
+				i--;
+			}
+		}
+
+		// xの値になるまでループ
+		for (int i = 0; i <= x; i++) {
+			// iを表示する
+			System.out.println(i);
+		}
+
+	}
+
+}
