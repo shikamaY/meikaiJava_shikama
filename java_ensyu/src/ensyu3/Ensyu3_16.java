c7177664612b9fa9aa679820b027cc4cfc61adfe
yuto-PC\yuto <ddt.shikama@gmail.com>
2017-12-20 21:26:22 +0900

    演習3-16 指摘反映（条件の簡略化）

--- java_ensyu/src/ensyu3/Ensyu3_16.java
+++ java_ensyu/src/ensyu3/Ensyu3_16.java
@@ -20,60 +20,47 @@ public class Ensyu3_16 {
 		int numC  = stdIn.nextInt();
 		// 最大値用変数
 		int max;
+		// maxに仮の値を入れる
+		max = numA;
 		// 中央値用変数
 		int center;
+		// centerに仮の値を入れる
+		center = numB;
 		// 最小値用変数
 		int min;
-		// numAがもっとも大きいかを比較する
-		if(numA > numB && numA > numC){
-			// 最大値にnumAを代入する
-			max = numA;
-			// numBがnumCより大きいかを比較する
-			if(numB > numC) {
-				// centerにnumBを代入する
-				center = numB;
-				// minにnumCを代入する
-				min = numC;
-			// numCがnumBより大きい場合
-			} else {
-				// centerにnumBを代入する
-				center = numC;
-				// minにnumBを代入する
-				min = numB;
-			}
-		// 	numBが最も大きいか比較する
-		} else if(numB > numA && numB > numC) {
-			// 最大値にnumBを代入する
-			max = numB;
-			// numAがnumCより大きいかを比較する
-			if(numA > numC) {
-				// centerにnumAを代入する
-				center = numA;
-				// minにnumCを代入する
-				min = numC;
-			// numCがnumAより大きい場合
-			} else {
-				// centerにnumCを代入する
-				center = numC;
-				// minにnumAを代入する
-				min = numA;
-			}
-		} else {
-			// 最大値にnumCを代入する
-			max = numC;
-			// numAがnumBより大きいかを比較する
-			if(numA > numB) {
-				// centerにnumAを代入する
-				center = numA;
-				// minにnumBを代入する
-				min = numB;
-			// numBがnumAより大きい場合
-			} else {
-				// centerにnumBを代入する
-				center = numB;
-				// minにnumAを代入する
-				min = numA;
-			}
+		// minに仮の値を入れる
+		min = numC;
+
+		// 入れ替えようの変数を用意する
+		int swapTmp;
+
+		// 最大値か判定する
+		if (max < center ) {
+			// 入れ替えよう変数に入れる
+			swapTmp = max;
+			// 最大値を入れ替える
+			max = center;
+			// 中央値を入れ替える
+			center = swapTmp;
+		}
+		// 最大値か判定する
+		if (max < min) {
+			// 入れ替えよう変数に入れる
+			swapTmp = max;
+			// 最大値を入れ替える
+			max = min;
+			// 最小値を入れ替える
+			min = swapTmp;
+		}
+
+		// 中央値を判定する
+		if (center < min) {
+			// 入れ替えよう変数に入れる
+			swapTmp = center;
+			// 中央値を入れ替える
+			center = min;
+			// 最小値を入れ替える
+			min = swapTmp;
 		}
 		// ソートしたとメッセージを表示する
 		System.out.println("ソート結果");
