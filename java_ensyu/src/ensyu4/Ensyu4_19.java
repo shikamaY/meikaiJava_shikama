99dbb88987322f42a360fde7d75251144413e1ba
yuto-PC\yuto <ddt.shikama@gmail.com>
2017-12-27 21:22:52 +0900

    演習4-19 指摘反映（コメントを追加）

--- java_ensyu/src/ensyu4/Ensyu4_19.java
+++ java_ensyu/src/ensyu4/Ensyu4_19.java
@@ -9,29 +9,48 @@ import java.util.Scanner;
 public class Ensyu4_19 {
 
 	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
 
+		// ユーザ入力のため、Scannerクラスのオブジェクトを生成
+		Scanner standardInput = new Scanner(System.in);
+
+		// リトライ判定用の変数
 		int retry;
+		// 入力された月を入れる変数
 		int month;
 
+		// リトライするためのループ
 		do {
+			// 入力値が不正の場合、再度入力を促すためにループする
 			do {
+				// 入力を促すメッセージを表示
 				System.out.print("季節を求めます。\n何月ですか：");
-				month=stdIn.nextInt();
+				// 入力値を代入する
+				month=standardInput.nextInt();
 			// 入力値が1～12であるかチェックする
 			} while (month <= 0 || month >= 13);
-
-			if (month >= 3 && month <= 5 )
+			// 入力値が3以上5以下の場合
+			if (month >= 3 && month <= 5 ) {
+				// 結果を表示する
 				System.out.println("それは春です。");
-			else if (month >= 6 && month <= 8)
+			// 入力値が6以上8以下の場合
+			} else if (month >= 6 && month <= 8) {
+				// 結果を表示する
 				System.out.println("それは夏です。");
-			else if (month >= 9 && month <= 11)
+			// 入力値が9以上11以下の場合
+			} else if (month >= 9 && month <= 11) {
+				// 結果を表示する
 				System.out.println("それは秋です。");
-			else if (month == 12 || month == 1 || month == 2)
+				// 入力値が12または1,2の場合
+			} else if (month == 12 || month == 1 || month == 2) {
+				// 結果を表示する
 				System.out.println("それは冬です。");
+			}
 
+			// リトライするか確認するメッセージを表示する
 			System.out.print("もう一度？1・・・Yes/0・・・No：");
-			retry = stdIn.nextInt();
+			// 入力値を代入する
+			retry = standardInput.nextInt();
+		// 1の場合はループする
 		} while (retry == 1);
 
 	}
