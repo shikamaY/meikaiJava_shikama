dc45b0d5b95ea4271d75cf290e0107542e31861d
yuto-PC\yuto <ddt.shikama@gmail.com>
2018-01-10 20:25:23 +0900

    演習5-3 完了

---
+++ java_ensyu/src/ensyu5/Ensyu5_3.java
@@ -0,0 +1,20 @@
+package ensyu5;
+
+/*演習5-3
+ * 論理型の変数にtrueやfalseを代入して、その値を表示するプログラムを作成せよ
+ */
+
+public class Ensyu5_3 {
+
+	public static void main(String[] args) {
+		// 論理型の値にtureを代入する
+		boolean resultTrue = true;
+		// 論理型の値にfalseを代入する
+		boolean resultFalse = false;
+		// trueを代入した結果を表示する
+		System.out.println("論理型のresultTrueの値は" + resultTrue);
+		// falseを代入した結果を表示する
+		System.out.println("論理型のresultFalseの値は" + resultFalse);
+	}
+
+}
