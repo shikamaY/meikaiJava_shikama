246ed7213184b9f2ffeb13f47e229a663a22f2aa
yuto-PC\yuto <ddt.shikama@gmail.com>
2018-01-31 21:08:37 +0900

    演習6-6 指摘反映（コメントを具体的にする）

--- java_ensyu/src/ensyu6/Ensyu6_6.java
+++ java_ensyu/src/ensyu6/Ensyu6_6.java
@@ -16,50 +16,50 @@ public class Ensyu6_6 {
 	public static void main(String[] args) {
 		// ユーザ入力のため、Scannerクラスのオブジェクトを生成する
 		Scanner standardInput = new Scanner(System.in);
-		// 何を入力するか表示する
+		// 人数を入力してもらうため、何を入力するか表示する
 		System.out.print(INPUT_MESSAGE);
-		// 入力値を代入
+		// 人数の入力値を代入
 		int inputNumber = standardInput.nextInt();
-		// 入力値分の配列を用意する
-		int scoreArray[] = new int[inputNumber];
+		// 得点を入れるため、人数分の配列を用意する
+		int testScoreArray[] = new int[inputNumber];
 		// 最高点を入れる変数
-		int max = 0;
+		int testScoreMax = 0;
 		// 最低点を入れる変数
-		int min = 0;
+		int testScoreMin = 0;
 
-		// 入力値分のループする
+		// 人数の入力値分のループする
 		for (int count = 0; count < inputNumber; count++) {
-			// 各要素に何を入れるか促すメッセージを表示する
+			// 得点の配列に入れるため、各要素に何を入れるか促すメッセージを表示する
 			System.out.print(count + 1 + INPUT_MESSAGE_NUMBER);
 			// 入力値を代入
-			scoreArray[count] = standardInput.nextInt();
+			testScoreArray[count] = standardInput.nextInt();
 			// 最大値の仮の値を代入
-			max = scoreArray[count];
+			testScoreMax = testScoreArray[count];
 			// 最小値の仮の値を代入
-			min = scoreArray[count];
+			testScoreMin = testScoreArray[count];
 		}
 		// 合計点を入れる変数
-		int sum = 0;
-		// 配列の要素分ループする
-		for (int sucore : scoreArray) {
+		int testScoreSum = 0;
+		// 合計値をと最高、最低点を計算するため人数分ループする
+		for (int sucore : testScoreArray) {
 			// 合計値に加算
-			sum += sucore;
+			testScoreSum += sucore;
 			// 最大値より大きいか判定する
-			if (max < sucore) {
+			if (testScoreMax < sucore) {
 				// 最大値に代入
-				max =  sucore;
+				testScoreMax =  sucore;
 			// 最小値より小さいか判定する
-			} else if (min > sucore) {
+			} else if (testScoreMin > sucore) {
 				// 最小値に代入する
-				min =  sucore;
+				testScoreMin =  sucore;
 			}
 		}
 		// 平均値を計算
-		double ave = (double)sum /(double)inputNumber;
+		double testScoreAve = (double)testScoreSum /(double)inputNumber;
 		// 合計と平均を表示する
-		System.out.println("合計点は" + sum + "です。平均点は" + ave + "です。");
+		System.out.println("合計点は" + testScoreSum + "です。平均点は" + testScoreAve + "です。");
 		// 最高点と最低点を表示する
-		System.out.println("最高点は" + max + "です。最低点は" + min + "です。");
+		System.out.println("最高点は" + testScoreMax + "です。最低点は" + testScoreMin + "です。");
 	}
 
 }
