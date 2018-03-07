b773db35950816add6bfd59bac6be358d492ed16
yuto-PC\yuto <ddt.shikama@gmail.com>
2018-03-07 21:10:57 +0900

    演習7-5 完了

---
+++ java_ensyu/src/ensyu7/Ensyu7_5.java
@@ -0,0 +1,15 @@
+package ensyu7;
+
+/* 演習7-5
+ * 「こんにちは。」と表示して改行するメソッドhelloを作成せよ
+ */
+public class Ensyu7_5 {
+
+	public static void main(String[] args) {
+		hello();
+	}
+
+	static void hello() {
+		System.out.println("こんにちは。");
+	}
+}
