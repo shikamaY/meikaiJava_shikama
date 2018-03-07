f2a2c3d64a3906c9f3ba8f6476907b10cdd1bf96
yuto-PC\yuto <ddt.shikama@gmail.com>
2018-03-07 20:55:18 +0900

    演習7-1 指摘反映（変数名の修正、returnの数を1つに変更）

--- java_ensyu/src/ensyu7/Ensyu7_1.java
+++ java_ensyu/src/ensyu7/Ensyu7_1.java
@@ -26,19 +26,20 @@ public class Ensyu7_1 {
 	/* 引数の数値が正なら1、負なら-1、0なら0を返すメソッド
 	 * number:判定する数値
 	 */
-	static int signOf(int number){
-
+	static int signOf(int checkNumber){
+		// メソッドの返り値を入れる変数を用意する
+		int returnCode = 0;
 		// 引数が正か判定する
-		if (number > 0) {
+		if (checkNumber > 0) {
 			// 正の場合は1を返す
-			return 1;
+			returnCode = 1;
 		// 引数が負か判定する
-		} else if (number < 0) {
+		} else if (checkNumber < 0) {
 			// 負の場合は-1を返す
-			return -1;
+			returnCode = -1;
 		}
 		// 0の場合は0を返す
-		return 0;
+		return returnCode;
 	}
 
 }
