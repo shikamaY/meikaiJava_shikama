681b75c3a0659a87f0dfdcaa2599bb4c56c6ed8e
yuto-PC\yuto <ddt.shikama@gmail.com>
2018-03-07 21:18:59 +0900

    演習7-5 コメント追記

--- java_ensyu/src/ensyu7/Ensyu7_5.java
+++ java_ensyu/src/ensyu7/Ensyu7_5.java
@@ -6,10 +6,13 @@ package ensyu7;
 public class Ensyu7_5 {
 
 	public static void main(String[] args) {
+		// 文字を出力するため、helloメソッドを呼び出す
 		hello();
 	}
 
+	// 「こんにちは。」を表示して改行するメソッド
 	static void hello() {
+		// 文字を表示して改行する
 		System.out.println("こんにちは。");
 	}
 }
