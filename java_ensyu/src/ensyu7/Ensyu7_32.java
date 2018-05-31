7a87d00e7eb9780fadab7c9347fde71f3c984b06
yuto-PC\yuto <ddt.shikama@gmail.com>
2018-05-31 20:49:13 +0900

    演習7-32 完了

---
+++ java_ensyu/src/ensyu7/Ensyu7_32.java
@@ -0,0 +1,87 @@
+package ensyu7;
+
+/* 数値の内部ビットを表示するメソッド群を作成せよ
+ */
+
+public class Ensyu7_32 {
+
+	public static void main(String[] args) {
+
+		// byte型の変数を用意する
+		byte byteNumber = 10;
+
+		printBits(byteNumber);
+		// short型の変数を用意する
+		short shortNumber = 3000;
+		// 数値をビット表示する
+		printBits(shortNumber);
+		// int型の変数を用意する
+		int intNumber = 10000;
+		// 数値をビット表示する
+		printBits(intNumber);
+		// long型の変数を用意する
+		long longNumber = 1000000000000000L;
+		// 数値をビット表示する
+		printBits(longNumber);
+	}
+
+	/* byte型の数値をビット表示するメソッド
+	 * number:ビット表示する数値
+	 */
+	static void printBits(byte number) {
+		// 何の型の処理か出
+		System.out.println("byte");
+		// 32桁分ループする
+		for (int count = 31; count >=0; count--) {
+			// 0か1を判定する
+			System.out.print(((number >>> count & 1) == 1) ? '1' : '0');
+		}
+		// 改行する
+		System.out.println();
+	}
+
+	/* short型の数値をビット表示するメソッド
+	 * number:ビット表示する数値
+	 */
+	static void printBits(short number) {
+		// 何の型の処理か出
+		System.out.println("short");
+		// 32桁分ループする
+		for (int count = 31; count >=0; count--) {
+			// 0か1を判定する
+			System.out.print(((number >>> count & 1) == 1) ? '1' : '0');
+		}
+		// 改行する
+		System.out.println();
+	}
+
+	/* int型の数値をビット表示するメソッド
+	 * number:ビット表示する数値
+	 */
+	static void printBits(int number) {
+		// 何の型の処理か出
+		System.out.println("int");
+		// 32桁分ループする
+		for (int count = 31; count >=0; count--) {
+			// 0か1を判定する
+			System.out.print(((number >>> count & 1) == 1) ? '1' : '0');
+		}
+		// 改行する
+		System.out.println();
+	}
+
+	/* long型の数値をビット表示するメソッド
+	 * number:ビット表示する数値
+	 */
+	static void printBits(long number) {
+		// 何の型の処理か出
+		System.out.println("long");
+		// 32桁分ループする
+		for (int count = 31; count >=0; count--) {
+			// 0か1を判定する
+			System.out.print(((number >>> count & 1) == 1) ? '1' : '0');
+		}
+		// 改行する
+		System.out.println();
+	}
+}
