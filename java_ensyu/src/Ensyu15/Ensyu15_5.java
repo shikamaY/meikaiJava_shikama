2e0cc04ce3af22b3fcd863813b8bc08ed9299bf0
yuto-PC\yuto <ddt.shikama@gmail.com>
2018-12-20 21:03:43 +0900

    演習15-5 指摘反映（15-6にて指摘を受けたため15-6でコミット）

--- java_ensyu/src/Ensyu15/Ensyu15_5.java
+++ java_ensyu/src/Ensyu15/Ensyu15_5.java
@@ -9,6 +9,16 @@ package Ensyu15;
 public class Ensyu15_5 {
 
 	public static void main(String[] args) {
+
+		// 勝ち判定用の定数
+		final int WIN = 2;
+		// 負け判定用の定数
+		final int LOSE = 1;
+		// 引き分け判定用の定数
+		final int DRAW = 0;
+		// 勝敗判定計算用の定数
+		final int JUDGE_NUMBER = 3;
+
 		// 人間クラスのインスタンスを生成
 		Human humanA = new Human();
 		// コンピュータクラスのインスタンスを生成
@@ -30,29 +40,32 @@ public class Ensyu15_5 {
 		// コンピュータ手を判定用変数にいれる
 		int computerBHand = computerB.getHand();
 		// 全員違う手の場合
-		if (humanAHand + computerAHand + computerBHand == 3) {
+		if (humanAHand + computerAHand + computerBHand == JUDGE_NUMBER) {
 			// 結果を表示する
 			System.out.println("引き分けです");
 		// それ以外の場合
 		} else {
 			// 勝敗を判定する
-			int judgeA = (humanAHand - computerAHand + 3) % 3;
+			int judgeA = (humanAHand - computerAHand + JUDGE_NUMBER) % JUDGE_NUMBER;
 			// 勝敗を判定する
-			int judgeB = (humanAHand - computerBHand + 3) % 3;
+			int judgeB = (humanAHand - computerBHand + JUDGE_NUMBER) % JUDGE_NUMBER;
 			// どちらも引き分けの場合
-			if (judgeA == 0 && judgeB == 0) {
+			if (judgeA == DRAW && judgeB == DRAW) {
 				// 結果を表示する
 				System.out.println("引き分けです");
 			// 両方に勝っている場合
-			} else if (judgeA == 2 && judgeB == 2) {
+			} else if (judgeA == WIN && judgeB == WIN) {
 				// 結果を表示する
 				System.out.println("勝ちです");
 			// 片方に勝っている場合
-			} else if(judgeA == 2 || judgeB == 2) {
+			} else if (judgeA == WIN || judgeB == WIN) {
 				// 結果を表示する
 				System.out.println("片方には勝ちました");
 			// 両方に負けている場合
-			} else {
+			}else if ((judgeA == DRAW && judgeB == LOSE) || (judgeA == LOSE && judgeB == DRAW)) {
+				// 結果を表示する
+				System.out.println("片方に負けです");
+			}else {
 				// 結果を表示する
 				System.out.println("負けです");
 			}
